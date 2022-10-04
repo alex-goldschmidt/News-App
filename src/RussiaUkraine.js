4da8c1a6e7bbd300687b89ca778ff322f8a474cc
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2022-10-03 21:02:28 -0400

    Create container for stories in ukraine category

--- src/RussiaUkraine.js
+++ src/RussiaUkraine.js
@@ -2,10 +2,36 @@ export const RussiaUkraine = (() => {
   const container = document.querySelector(".container");
 
   const categoryHeader = document.createElement("div");
-
   categoryHeader.innerHTML = `
   <div class="RussiaUkraineHeader" data-id="RussiaUkraineHeader">
   <div class="title">Russia and Ukraine</div>
   <div class="buttonContainer" data-id="buttonContainer">Back</div>`;
+
   container.appendChild(categoryHeader);
+
+  const categoryStories = document.createElement("div");
+  categoryStories.classList.add("RussiaUkraineStories");
+  categoryStories.innerHTML = `
+  <div class="RukStoriesBlock" data-id="RukStoriesBlock">
+          <div class="rukLeft">
+            <div class="lean">Left</div>
+            <div class="rukLeftOrganization" data-id="rukLeftOrganization">Organization</div>
+            <img class="rukLeftPicture" src="" alt="" data-id="rukLeftPicture"/>
+            <div class="rukLeftArticle" data-id="rukLeftArticle">This is an article</div> 
+          </div>    
+          <div class="rukCenter">
+            <div class="lean">Center</div>
+            <div>Organization</div>
+            <img src="" alt="" />
+            <div>This is an article</div>
+          </div>  
+          <div class="rukRight">
+            <div class="lean">Right</div>
+            <div>Organization</div>
+            <img src="" alt="" />
+            <div>This is an article</div>
+          </div>  
+        </div>
+  `;
+  container.appendChild(categoryStories);
 })();
--- src/index.js
+++ src/index.js
@@ -5,14 +5,7 @@ import { Economy } from "./economy";
 import { Congress } from "./congress";
 import { China } from "./china";
 
-const BackButton = document.querySelectorAll(`[data-id="buttonContainer"]`);
-const RussiaUkraineBox = document.querySelector(`[data-id="RussiaUkraine"]`);
-const BigTechBox = document.querySelector(`[data-id="BigTech"]`);
-const EconomyBox = document.querySelector(`[data-id="Economy"]`);
-const CongressBox = document.querySelector(`[data-id="Congress"]`);
-const ChinaBox = document.querySelector(`[data-id="China"]`);
-const categories = document.querySelector(`[data-id="CategoryContent"]`);
-
+//headers for each page
 const mainHeader = document.querySelector(`[data-id="mainHeaderContent"]`);
 const RussiaUkraineHeader = document.querySelector(
   `[data-id="RussiaUkraineHeader"]`
@@ -21,9 +14,19 @@ const BigTechHeader = document.querySelector(`[data-id="BigTechHeader"]`);
 const EconomyHeader = document.querySelector(`[data-id="EconomyHeader"]`);
 const CongressHeader = document.querySelector(`[data-id="CongressHeader"]`);
 const ChinaHeader = document.querySelector(`[data-id="ChinaHeader"]`);
+//
+
+//cards on the home page
+const RussiaUkraineBox = document.querySelector(`[data-id="RussiaUkraine"]`);
+const BigTechBox = document.querySelector(`[data-id="BigTech"]`);
+const EconomyBox = document.querySelector(`[data-id="Economy"]`);
+const CongressBox = document.querySelector(`[data-id="Congress"]`);
+const ChinaBox = document.querySelector(`[data-id="China"]`);
+const categories = document.querySelector(`[data-id="CategoryContent"]`);
 
 RussiaUkraineBox.addEventListener("click", () => {
   RussiaUkraineHeader.style.display = "initial";
+  RukStoriesBlock.style.display = "flex";
   BigTechHeader.style.display = "none";
   EconomyHeader.style.display = "none";
   CongressHeader.style.display = "none";
@@ -71,7 +74,15 @@ ChinaBox.addEventListener("click", () => {
   categories.style.display = "none";
   mainHeader.style.display = "none";
 });
+//
 
+//stories card for each category's page
+const RukStoriesBlock = document.querySelector(`[data-id="RukStoriesBlock"]`);
+RukStoriesBlock.style.display = "none";
+//
+
+//targets back button to bring user back home, while clearing all other contents
+const BackButton = document.querySelectorAll(`[data-id="buttonContainer"]`);
 function AllBackButtons() {
   for (let i = 0; i < BackButton.length; i++) {
     BackButton[i].addEventListener("click", () => {
@@ -82,6 +93,7 @@ function AllBackButtons() {
       RussiaUkraineHeader.style.display = "none";
       ChinaHeader.style.display = "none";
       CongressHeader.style.display = "none";
+      RukStoriesBlock.style.display = "none";
     });
   }
 }
